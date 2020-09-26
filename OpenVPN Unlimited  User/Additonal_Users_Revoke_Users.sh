bef7fda3d06363e6d131c2f3a144db79cab10116
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2020-09-26 11:52:01 +0500

    Update Additonal_Users_Revoke_Users.sh

--- OpenVPN Unlimited  User/Additonal_Users_Revoke_Users.sh
+++ OpenVPN Unlimited  User/Additonal_Users_Revoke_Users.sh
@@ -8,7 +8,7 @@ export OVPN_DATA
 
 echo -e "\nMust type -----> no <------ in small letters, otherswise existing data will be lost from Database\n"
 
-docker run -v $OVPN_DATA:/etc/openvpn --rm -it kylemanna/openvpn ovpn_initpki
+docker run -v $OVPN_DATA:/etc/openvpn --rm -it quickbooks2018/openvpn ovpn_initpki
 
 sleep 1
 
@@ -18,14 +18,14 @@ echo -e "\nI am adding a client with name $CLIENTNAME\n"
 
 #docker run -v $OVPN_DATA:/etc/openvpn --rm -it kylemanna/openvpn easyrsa build-client-full $CLIENTNAME nopass
 
-docker run -v $OVPN_DATA:/etc/openvpn --rm -it kylemanna/openvpn easyrsa build-client-full $CLIENTNAME
+docker run -v $OVPN_DATA:/etc/openvpn --rm -it quickbooks2018/openvpn easyrsa build-client-full $CLIENTNAME
 
 echo -e "\nWe are now at 6TH Step, don't worry this is last step, you lazy GUY,Now we retrieve the client configuration with embedded certificates\n"
 
 
 echo -e "\n$CLIENTNAME ok\n"
 
-docker run -v $OVPN_DATA:/etc/openvpn --rm kylemanna/openvpn ovpn_getclient $CLIENTNAME > $CLIENTNAME.ovpn
+docker run -v $OVPN_DATA:/etc/openvpn --rm quickbooks2018/openvpn ovpn_getclient $CLIENTNAME > $CLIENTNAME.ovpn
 
 # END
 
