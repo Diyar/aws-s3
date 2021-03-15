c95d15b01befa31a43c0bd2d2d483aa917ed5004
root <root@ip-172-31-89-240.ec2.internal>
2021-03-15 01:58:23 +0000

    aws-azure-login Dockerfile source code

--- aws-azure-login/docker-launch.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker run --rm -it -v ~/.aws:/root/.aws sportradar/aws-azure-login "$@"
--- aws-azure-login/src/CLIError.ts
+++
@@ -1,8 +0,0 @@
-export class CLIError extends Error {
-  constructor(message: string) {
-    super(message);
-    Error.captureStackTrace(this, this.constructor);
-    this.name = this.constructor.name;
-    this.message = message;
-  }
-}
